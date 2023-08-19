31bcef20160d32b13f5eb728ab3fc82a7b2573f6
Eeshasr <eesharavindran@gmail.com>
2023-08-19 17:15:00 +0530

    this is my flask code

--- app.py
+++ app.py
@@ -2,9 +2,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+@app.route("/hello1")
+def hello_world1():
+    return "Hello, World1!"
+
+@app.route("/hello2")
+def hello_world2():
+    return "Hello, World2!"
+
+@app.route('/age_finder')
+def age():
+    a = 2023 - 1994
+     
+    return 'the age is {}'.format(a)
+
+from flask import request
+@app.route('/input')
+@app.route('/input')
+def input_req():
+    data = request.args.get('x')
+    data1 = request.args.get('y')
+    data3 = data1 + data
+    return 'This is the input: {}'.format(data3)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
